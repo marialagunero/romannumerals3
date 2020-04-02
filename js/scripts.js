dda496ca3d40d83ea2fdaafa22adae2cf0da0ebe
Maria Lagunero <maria58@gmail.com>
2020-04-02 15:43:39 -0700

    add var ones if else statements to check functionality code works for this piece

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,41 @@ var tooLarge = function(userNumber) {
     alert('sorry, your number is too large. Roman numerals can only count to 3,999');
   } else {
     alert('number is useable');
-    //translate(userNumber);
+    translate(userNumber);
   }
 };
 
 
 //Translate function: 
+var translate = function(x) {
 // 1. determine length of userInput
+  var length = x.length();
 // 2. if userInput.length is 1 digit, pass digit into 'ones' function.
+  if (length === 1){
+    onesFunction(x);
+  }
 // 3. else if userInput.length = 2, separate first and second digits. use 'ones' function on digit in ones position, use 'tens' function on digit in tens position. return digits and combine them back for userOutput
 // 4. else if userInput.length = 3, separate first,second and third digits. use 'ones' function on digit in ones position, use 'tens' function on digit in tens position, use 'hundreds' function on digit in hundreds position. return digits and combine them back for userOutput
 //5.  else userInput.length = 4, separate first,second, third and 4th digits. use 'ones' function on digit in ones position, use 'tens' function on digit in tens position, use 'hundreds' function on digit in hundreds position. use 'thousands' function on digit in the thousands position. return digits and combine them back for userOutput
+};
 
 //var ones = function(userInput){};
+var onesFunction = function(onesDigit){
+ if (onesDigit === 1){ 
+  alert('ones function is working kind of'+ onesDigit);
+  //return 'I';
+ } else if (onesDigit === 2) {
+   alert('onesFunction working when input is 2' + ' ' +onesDigit);
+   return 'II';
+ } else if (onesDigit === 3){
+   alert('onesFunction working when input is 3')
+   return 'III';
+ } else if (onesDigit === 4){
+   return 'IV';
+ }
+};
+
+onesFunction(3);
 
 //var tens = function(userInput){};
 
@@ -36,5 +58,11 @@ $(document).ready(function(){
     //var result = translate(userInput);
     var tooLargeOutput = tooLarge(userInput);
   });
-
 });
+
+// var ourFunction = function (parameter){ 
+// alert('hi');
+// alert(parameter);
+// };
+
+// ourFunction(userInput);
